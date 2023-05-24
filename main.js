c49336571976dae6084eb4aa85b8b434d154f530
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-05-23 22:40:52 -0500

    feat(Punto1Taller2): :sparkles: punto2

--- main.js
+++ main.js
@@ -3,18 +3,30 @@
 // mostrar un mensaje "Estudie“, de lo contrario un mensaje que
 // diga "becado"
 
-let myFormulario = document.querySelector("#myFormulario");
+let myFormulario = document.querySelector("#myFormularioEjer2");
 let myHeaders = new Headers({"Content-Type": "application/json"});
 let config = {
     headers: myHeaders, 
 };
-myFormulario.addEventListener("submit", async(e)=>{
+let myEjer2 = document.querySelector("#myFormularioEjer2");
+
+myEjer2.addEventListener("submit", async(e)=>{
     e.preventDefault();
-    config.method = "POST";
-    let data = Object.fromEntries(new FormData(e.target));
-    config.body = JSON.stringify(data);
-    console.log(data);
-    let res = await (await fetch("api.php", config)).text();
-    document.querySelector("pre").innerHTML = res;
-    console.log(res);
+    try{
+        let data = Object.fromEntries(new FormData(e.target));
+        config.method ="POST";
+        config.body = JSON.stringify(data);
+        let result = await (await fetch("api.php", config)).text();
+        result=JSON.parse(result);
+        let plantilla = 
+        `<div class="par-impar">
+            <p>${result.resultado}</p>
+        </div>`;
+        document.querySelector("#resultEjer2").innerHTML = plantilla;
+       
+    }catch(e){
+        console.log(e)
+        
+    }
+    
 })
